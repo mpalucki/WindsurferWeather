4970e34208fdcad2c38c8c09d773d9faf3dab81d
mpalucki <mat.palucki@gmail.com>
2025-03-11 22:46:53 +0100

    init spring to create windsurfer weather service

---
+++ src/test/java/com/example/windsurferweather/WindsurferWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.windsurferweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindsurferWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
